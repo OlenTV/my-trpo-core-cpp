e80f5471ac25a4beae2af40a95e44da0402b2ea4
OlenTV <gmagarov@mail.ru>
2021-03-30 21:31:14 +0300

    Main upload

---
+++ EquationInterface.cpp
@@ -0,0 +1,28 @@
+//
+//  EquationInterface.cpp
+//  aaa
+//
+//  Created by Mac on 23/10/2019.
+//  Copyright © 2019 Mac. All rights reserved.
+//
+
+
+#include <iostream>
+#include <sstream>
+#include <cmath>
+#include <vector>
+#include <stdlib.h>
+#include <exception>
+#include <string>
+
+
+using namespace std;
+namespace Core
+{
+    class EquationInterface
+    {
+    public:
+     
+        virtual vector<float>* solve (float a, float b, float c) = 0;
+    };
+}
